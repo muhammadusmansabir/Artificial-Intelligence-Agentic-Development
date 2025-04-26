d66bb46ee7cc9b94b0b24e410fbe5379cec29fc7
iCode-Stack <usman261295@gmail.com>
2025-04-26 18:47:19 +0500

    code push

---
+++ fastapi-postgres/config/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+DATABASE_URL = os.getenv("POSTGRES_URI")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- fastapi-postgres/models/todo_model.py
+++ fastapi-postgres/models/todo_model.py
@@ -1,13 +1,26 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
-from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
+
+
+class UserModel(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, nullable=False,unique=True, index=True)
+    password = Column(String)
+    
+
 
 class TodoModel(Base):
     __tablename__ = 'todos'
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
+    title = Column(String, index=True, nullable=True)
     description = Column(String, index=True)
+    status = Column(String, index=True)
     completed = Column(Boolean, default=False)
-    created_at = Column(DateTime, default=datatime.datatime.now().strftime('%Y-%m-%d %H:%M:%S')) 
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
     
+ 
